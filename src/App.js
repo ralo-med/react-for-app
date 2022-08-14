1ac75c028b5218bffebe9d081037c28aae0ce9bf
Ralo <74175604+ralo-med@users.noreply.github.com>
2022-08-14 20:55:35 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React, { useState, useEffect } from "react";
 import Button from "./Button";
 import styles from "./App.module.css";
 
+function Hello() {
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+    //이게 cleanup function - > 거의 안씀
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
   const onClick = () => setValue((prev) => prev + 1);
+  const onClick2 = () => setShowing((prev) => !prev);
   const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
@@ -28,7 +39,11 @@ function App() {
       <h1 className={styles.title}>Welcome back!!!</h1>
       <button onClick={onClick}>clcik me</button>
       <h2>{counter}</h2>
-      <Button text={"Continue"} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
+      <div>
+        <Button text={"Continue"} />
+      </div>
     </div>
   );
 }
