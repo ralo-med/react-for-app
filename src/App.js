5f23e50b22ce6b78d1c3ec3adccdd16ab858d1a1
Captainna <leona8464@gmail.com>
2022-08-14 19:47:16 +0900

    5.2 ing

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import styles from "./App.module.css";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => (setKeyword = event.target.value);
   console.log("I run all the time");
 
   useEffect(() => {
@@ -12,6 +14,12 @@ function App() {
   }, []);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1 className={styles.title}>Welcome back!!!</h1>
       <button onClick={onClick}>clcik me</button>
       <h2>{counter}</h2>
