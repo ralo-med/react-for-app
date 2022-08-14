768dd2775b2838617e0f27924dcc201a0c110df0
Ralo <74175604+ralo-med@users.noreply.github.com>
2022-08-14 20:24:22 +0900

    #6.2 Deps

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => (setKeyword = event.target.value);
-  console.log("I run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
 
   useEffect(() => {
-    console.log("CALL THE API....");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]);
   return (
     <div>
       <input
