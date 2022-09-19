184c0fcb5c89538557f5f0cd4632ffe5008c079d
Ralo <74175604+ralo-med@users.noreply.github.com>
2022-09-20 01:04:30 +0900

    #7.1 To Do List part Two

--- src/App.js
+++ src/App.js
@@ -1,51 +1,39 @@
-import React, { useState, useEffect } from "react";
-import Button from "./Button";
-import styles from "./App.module.css";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-    //이게 cleanup function - > 거의 안씀
-  }, []);
-  return <h1>Hello</h1>;
-}
+import React, { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setValue((prev) => prev + 1);
-  const onClick2 = () => setShowing((prev) => !prev);
-  const onChange = (event) => setKeyword(event.target.value);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
 
-  useEffect(() => {
-    console.log("I run only once");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+
+  console.log(toDos);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <button onClick={onClick}>clcik me</button>
-      <h2>{counter}</h2>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
-      <div>
-        <Button text={"Continue"} />
-      </div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do ..."
+        />
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <button>Add To Do</button>
     </div>
   );
 }
-
 export default App;
